b6f764b5bca872f6bce313749d9a0d1f55f940ed
Tigers5566 <Timur11724@gmail.com>
2025-05-04 14:02:23 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,41 @@
-
 from keras.models import load_model  # TensorFlow is required for Keras to work
 from PIL import Image, ImageOps  # Install pillow instead of PIL
 import numpy as np
 
-# Disable scientific notation for clarity
+import telebot
+bot = telebot.TeleBot("Bot Token")
+
 np.set_printoptions(suppress=True)
-
-# Load the model
-model = load_model("keras_model.h5", compile=False)
-
-# Load the labels
+model = load_model("keras_Model.h5", compile=False)
 class_names = open("labels.txt", "r").readlines()
+@bot.message_handler(content_types=['photo'])
+def detected(message):
+    file_info = bot.get_file(message.photo[-1].file_id)
+    file_name = file_info.file_path.split('/')[-1]
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(file_name, 'wb') as new_file:
+        new_file.write(downloaded_file)
 
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+            
+        
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+        image = Image.open(file_name).convert("RGB")
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+ 
+    image_array = np.asarray(image)
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+    data[0] = normalized_image_array
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]
 
-# Replace this with the path to your image
-image = Image.open("1858836.jpg").convert("RGB")
+    bot.send_message(message.chat.id, f'"Относится к:" {class_name[2:]}')
+    bot.send_message(message.chat.id, f'"С вероятностью в:" {confidence_score}')
 
-# resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+    
 
-# turn the image into a numpy array
-image_array = np.asarray(image)
 
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
 
-# Load the image into the array
-data[0] = normalized_image_array
-
-# Predicts the model
-prediction = model.predict(data)
-index = np.argmax(prediction)
-class_name = class_names[index]
-confidence_score = prediction[0][index]
-
-# Print prediction and confidence score
-print("Class:", class_name[2:], end="")
-print("Confidence Score:", confidence_score)
+bot.infinity_polling()
